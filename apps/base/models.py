64f14b00a5b45417f22d65615de97882cd3030c8
Hale Addas <59157448+hala415@users.noreply.github.com>
2022-05-27 12:30:50 -0700

    third commit

---
+++ apps/base/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, Group as DjangoGroup
+from django.utils.translation import ugettext_lazy as _
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content= models.TextField()
+    def __str__(self):
+        return self.title
+
+
+#class Users(User.AbstractBaseUser):
+    #                              V   "_" ?????
+    #first_name =  models.CharField(_("first name"), max_length=30)
+    #last_name =  models.CharField(_("last name"), max_length=30)
+    #email = models.EmailField(verbose_name=_("email address"), max_length=255, unique=True, null=True, blank=True)
+    #isstaff =  models.BooleanField(_("staff status"), default=False, help_text=_("Designates whether the user can log into this admin site."))
+    #isactive =  models.BooleanField(_("active"), default=True, help_text=_("is active"))
+
+
+class Company():
+    company_name = models.CharField(_("company name"), max_length=50)
+ 
